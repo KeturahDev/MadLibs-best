5d166993d48f0771acdada7c721df0dd0e2f4f72
Keturah Howard and Kristina Hengster Tintor <keturah+kristina@>
2020-03-09 13:48:53 -0700

    create new homepage that directs to multiple forms, which leads to disignated story

--- Madlibs/Controllers/HomeController.cs
+++ Madlibs/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ namespace MadLibs.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
+    public ActionResult StoryDirectory() { return View(); }
+
+    [Route("/story-form")]
     public ActionResult Form() { return View(); }
     [Route("/story")]
     public ActionResult Story(string name, string animal, string bathroomTask, string reflectiveObject)
@@ -16,6 +19,24 @@ namespace MadLibs.Controllers
       myStoryVariables.BathroomTask = bathroomTask;
       myStoryVariables.ReflectiveObject = reflectiveObject;
       return View(myStoryVariables);   
-      }
+    }
+    [Route("/epicodus-story-form")]
+    public ActionResult EpicodusStoryForm() { return View(); }
+
+    [Route("/epicodus-story")]
+    public ActionResult EpicodusStory (string weirdName, string codingCatastrophy, string timeOfYear, string programmingLang, string studentName, string hobby)
+    {
+      EpicodusStoryVariables myVariables = new EpicodusStoryVariables();
+      myVariables.WeirdName = weirdName;
+      myVariables.CodingCatastrophy = codingCatastrophy;
+      myVariables.TimeOfYear = timeOfYear;
+      myVariables.ProgrammingLang = programmingLang;
+      myVariables.StudentName = studentName;
+      myVariables.Hobby = hobby;
+      return View(myVariables);
+    } 
   }
 }
+  
+
+  
---
+++ Madlibs/Models/EpicodusStoryVariables.cs
@@ -0,0 +1,19 @@
+namespace MadLibs.Models
+{
+  public class EpicodusStoryVariables
+  {
+    public string WeirdName {get;set;}
+    public string CodingCatastrophy {get;set;}
+    public string TimeOfYear {get;set;}
+    public string ProgrammingLang {get;set;}
+    public string StudentName {get;set;}
+    public string Hobby {get;set;}
+  }
+}
+
+// <!-- new teacher (weird name) -->
+// <!-- coding catastrophy -->
+// <!-- time of year -->
+// <!-- Programming language -->
+// <!-- Person1 -->
+// <!-- Hobby -->
