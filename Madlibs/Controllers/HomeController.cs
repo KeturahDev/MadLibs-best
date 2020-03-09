718dcbd497e240353b9117990df36b124c65d21c
Keturah Howard and Kristina Hengster Tintor <keturah@>
2020-03-09 11:24:20 -0700

    create more changable variables

--- Madlibs/Controllers/HomeController.cs
+++ Madlibs/Controllers/HomeController.cs
@@ -5,6 +5,17 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/")]
     public ActionResult Form() { return View(); }
+    [Route("/story")]
+    public ActionResult Story(string name, string animal, string bathroomTask, string reflectiveObject)
+    {
+      StoryVariables myStoryVariables = new StoryVariables();
+      myStoryVariables.Name = name;
+      myStoryVariables.Animal = animal;
+      myStoryVariables.BathroomTask = bathroomTask;
+      myStoryVariables.ReflectiveObject = reflectiveObject;
+      return View(myStoryVariables);   
+      }
   }
 }
--- Madlibs/Models/StoryVariables.cs
+++ Madlibs/Models/StoryVariables.cs
@@ -4,5 +4,7 @@ namespace MadLibs.Models
     {
         public string Name {get; set;}
         public string Animal {get; set;}
+        public string ReflectiveObject { get; set; }
+        public string BathroomTask { get; set; }
     }
 }
